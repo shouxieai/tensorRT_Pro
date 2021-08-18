e2aa7a3852ed823bddf6a640ba3f395022711f13
Wish <dujw@deepblueai.com>
2021-08-18 10:31:33 +0800

    update api

--- src/tensorRT/infer/trt_infer.hpp
+++ src/tensorRT/infer/trt_infer.hpp
@@ -13,25 +13,26 @@ namespace TRT {
 
 	class Infer {
 	public:
-		virtual void forward(bool sync = true) = 0;
-		virtual int get_max_batch_size() = 0;
+		// 执行forward推理前，请把数据输入到input中，确保其shape有效
+		virtual void     forward(bool sync = true) = 0;
+		virtual int      get_max_batch_size() = 0;
+		virtual void     set_stream(CUStream stream) = 0;
 		virtual CUStream get_stream() = 0;
-		virtual void set_stream(CUStream stream) = 0;
-		virtual void synchronize() = 0;
-		virtual size_t get_device_memory_size() = 0;
+		virtual void     synchronize() = 0;
+		virtual size_t   get_device_memory_size() = 0;
+		virtual bool     is_dynamic_batch_dimension() = 0;
 		virtual std::shared_ptr<MixMemory> get_workspace() = 0;
-		virtual bool is_dynamic_batch_dimension() = 0;
-		virtual std::shared_ptr<Tensor> input(int index = 0) = 0;
-		virtual std::string get_input_name(int index = 0) = 0;
-		virtual std::shared_ptr<Tensor> output(int index = 0) = 0;
+		virtual std::shared_ptr<Tensor>    input (int index = 0) = 0;
+		virtual std::shared_ptr<Tensor>    output(int index = 0) = 0;
+		virtual std::shared_ptr<Tensor>    tensor(const std::string& name) = 0;
+		virtual std::string get_input_name (int index = 0) = 0;
 		virtual std::string get_output_name(int index = 0) = 0;
 		virtual bool is_output_name(const std::string& name) = 0;
-		virtual bool is_input_name(const std::string& name) = 0;
-		virtual std::shared_ptr<Tensor> tensor(const std::string& name) = 0;
-		virtual int num_output() = 0;
-		virtual int num_input() = 0;
+		virtual bool is_input_name (const std::string& name) = 0;
+		virtual int  num_output() = 0;
+		virtual int  num_input() = 0;
 		virtual void print() = 0;
-		virtual int device() = 0;
+		virtual int  device() = 0;
 	};
 
 	struct DeviceMemorySummary {
