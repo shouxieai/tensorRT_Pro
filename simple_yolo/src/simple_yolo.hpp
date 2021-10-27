57243e20b6af951968d51e619442e5a1a43e456f
Wish <dujw@deepblueai.com>
2021-10-27 10:40:41 +0800

    update commit

--- simple_yolo/src/simple_yolo.hpp
+++ simple_yolo/src/simple_yolo.hpp
@@ -48,6 +48,15 @@ namespace SimpleYolo{
     const char* mode_string(Mode type);
     void set_device(int device_id);
 
+    /* 
+        模型编译
+        max batch size：为最大可以允许的batch数量
+        source_onnx：仅仅只支持onnx格式输入
+        saveto：储存的tensorRT模型，用于后续的加载
+        max workspace size：最大工作空间大小，一般给1GB，在嵌入式可以改为256MB，单位是byte
+        int8 images folder：对于Mode为INT8时，需要提供图像数据进行标定，请提供文件夹，会自动检索下面的jpg/jpeg/tiff/png/bmp
+        int8_entropy_calibrator_cache_file：对于int8模式下，熵文件可以缓存，避免二次加载数据，可以跨平台使用，是一个txt文件
+     */
     // 1GB = 1<<30
     bool compile(
         Mode mode, Type type,
