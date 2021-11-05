157be8ba292a3c13f2ab90216d511f3933b5b5de
Wish <dujw@deepblueai.com>
2021-11-05 22:56:51 +0800

    add multi gpu yolo

--- src/application/app_yolo/multi_gpu.cpp
+++ src/application/app_yolo/multi_gpu.cpp
@@ -49,9 +49,7 @@ namespace Yolo{
     class BalancedImpl : public MultiGPUInfer, public MultiGPUInferImpl{
     public:
         int get_gpu_index(){
-            int gpu_index = (cursor_ + 1) % infers_.size();
-            cursor_++;
-            return gpu_index;
+            return ((cursor_++) + 1) % infers_.size();
         }
 
         virtual shared_future<BoxArray> commit(const cv::Mat& image) override{
@@ -66,66 +64,12 @@ namespace Yolo{
         atomic<unsigned int> cursor_{0};
     };
 
-    class ThreadIDHashImpl : public MultiGPUInfer, public MultiGPUInferImpl{
-    public:
-        unsigned int get_hash_index(const void* key, size_t length, size_t table_size){
-
-            const unsigned char* p = (const unsigned char*)key;
-            unsigned int hash_value = 0;
-            for(int i = 0; i < length; ++i)
-                hash_value = (hash_value << 5) + *p++;
-            
-            return hash_value % table_size;
-        }
-
-        int get_gpu_index(){
-            std::ostringstream oss;
-            oss << std::this_thread::get_id();
-            std::string stid = oss.str();
-            return get_hash_index(stid.c_str(), stid.size(), infers_.size());
-        }
-
-        virtual shared_future<BoxArray> commit(const cv::Mat& image) override{
-            return infers_[get_gpu_index()]->commit(image);
-        }
-
-        virtual vector<shared_future<BoxArray>> commits(const vector<cv::Mat>& images) override{
-            return infers_[get_gpu_index()]->commits(images);
-        }
-    };
-
-    // class ThreadPoolImpl : public MultiGPUInfer, public MultiGPUInferImpl{
-    // public:
-    //     virtual shared_future<BoxArray> commit(const cv::Mat& image) override{
-
-    //     }
-
-    //     virtual vector<shared_future<BoxArray>> commits(const vector<cv::Mat>& images) override{
-
-    //     }
-    // };
-
     shared_ptr<MultiGPUInfer> create_multi_gpu_infer(
         const string& engine_file, Type type, const vector<int> gpuids, 
-        DispatchMethod dispatch_method,
         float confidence_threshold, float nms_threshold,
         NMSMethod nms_method, int max_objects
     ){
-        shared_ptr<MultiGPUInfer> instance;
-        switch(dispatch_method){
-        case DispatchMethod::Balanced: instance.reset(new BalancedImpl());         break;
-        case DispatchMethod::ThreadIDHash: instance.reset(new ThreadIDHashImpl()); break;
-        //case DispatchMethod::ThreadPool: instance.reset(new ThreadPoolImpl());     break;
-        case DispatchMethod::ThreadPool: 
-            INFOE("Not implemented method");
-            break;
-        }
-
-        if(instance == nullptr){
-            INFOE("Unknow dispatch method");
-            return instance;
-        }
-
+        shared_ptr<MultiGPUInfer> instance(new BalancedImpl());
         auto impl = std::dynamic_pointer_cast<MultiGPUInferImpl>(instance);
         if(!impl->startup(
             engine_file, type, gpuids, confidence_threshold, nms_threshold, nms_method, max_objects
--- src/application/app_yolo/multi_gpu.hpp
+++ src/application/app_yolo/multi_gpu.hpp
@@ -5,18 +5,10 @@
 
 namespace Yolo{
 
-    enum class DispatchMethod{
-        Balanced     = 0,  /* commit阻塞，均衡的为每一个GPU分配任务，顺序的进行 */
-        ThreadIDHash = 1,  /* commit阻塞，按照请求者的线程id，进行hash后选择gpu，同一个线程所有请求都送往同一个gpu */
-        ThreadPool   = 2   /* commit不阻塞，线程池，commit函数立刻返回，由线程池内进行阻塞，好处是单个线程可以调度多
-                              gpu。请保证请求的image不会立马修改 */
-    };
-
     class MultiGPUInfer : public Yolo::Infer{};
 
     shared_ptr<MultiGPUInfer> create_multi_gpu_infer(
         const string& engine_file, Type type, const vector<int> gpuids, 
-        DispatchMethod dispatch_method = DispatchMethod::Balanced,
         float confidence_threshold=0.25f, float nms_threshold=0.5f,
         NMSMethod nms_method = NMSMethod::FastGPU, int max_objects = 1024
     );
